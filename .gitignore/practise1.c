69c2db43f36395cba7307d3aa84fe09690da83b6
karthi45 <30342460+karthi45@users.noreply.github.com>
2017-07-29 06:39:37 -0700

    Create practise1.c

---
+++ .gitignore/practise1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the value of num");
+scanf("%d",&num);
+if (num<0)
+printf("ENTERED VALUE IS NEGATIVE");
+else if(num>0)
+printf("ENTERED VALUE IS POSITIVE");
+else
+printf("ENTERED VALUE IS zero");
+return 0;
+}
