66a299e47065fd740f9791276f69f666f741b2ff
karthi45 <30342460+karthi45@users.noreply.github.com>
2017-07-29 06:58:53 -0700

    Create oddeven.c

---
+++ .gitignore/oddeven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num;
+scanf("%d",&num);
+if (num==0)
+printf("THE NUMBER IS NEITHER OOD OR EVEN");
+if (num%2==0)
+printf("THE NUMBER IS EVEN");
+else
+printf("THE NUMBER IS ODD");
+return 0;
+}
